2bb0e951d8734c77f0558b4a36c8a601fcf47052
tejasvig123 <89226632+tejasvig123@users.noreply.github.com>
2026-05-11 19:32:40 -0500

    Added initializing and scale down of image

--- src/project.py
+++ src/project.py
@@ -3,3 +3,32 @@ from PIL import Image
 import sys
 import random
 import os
+
+class GlitchSynthesizer:
+    def __init__(self, image_path):
+        pygame.init()
+        
+        # Navigate up one directory since project.py is in /src/
+        # but test.jpg is in the root directory.
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        full_image_path = os.path.join(root_dir, image_path)
+
+        try:
+            self.master_image = Image.open(full_image_path).convert("RGB")
+        except FileNotFoundError:
+            print(f"CRITICAL ERROR: Could not find '{image_path}' in the root folder.")
+            sys.exit()
+
+        # scale down if image is too large
+        width, height = self.master_image.size
+        if width > 1200 or height > 800:
+            self.master_image.thumbnail((1200, 800))
+            width, height = self.master_image.size
+
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Algorithmic Glitch Synthesizer")
+        self.font = pygame.font.SysFont("Courier", 24, bold=True)
+
+    
